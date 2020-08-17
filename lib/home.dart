df7922407ef110943ee54612a503d2a1936b81e7
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-17 12:37:02 -0300

    Final AppBar, Inicio PageView

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:nubankinterface/widgets/nu_app_bar.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +23,27 @@ class Home extends StatelessWidget {
       body: Stack(
         alignment: Alignment.topCenter,
         children: <Widget>[
-          nuAppBar(),
+          NuAppBar(
+            showMenu: _showMenu,
+            onTap: () {
+              setState(() {
+                _showMenu = !_showMenu;
+              });
+            },
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * .20,
+            height: MediaQuery.of(context).size.height * .55,
+            left: 0,
+            right: 0,
+            child: PageView(
+              children: <Widget>[
+                Container(color: Colors.amber,),
+                Container(color: Colors.black,),
+                Container(color: Colors.blue,),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/widgets/nu_app_bar.dart
+++ lib/widgets/nu_app_bar.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class nuAppBar extends StatelessWidget {
+class NuAppBar extends StatelessWidget {
+
+  final bool showMenu;
+  final VoidCallback onTap;
+  const NuAppBar({
+    Key key,
+    this.showMenu,
+    this.onTap
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,33 +18,38 @@ class nuAppBar extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).padding.top,
         ),
-        Container(
-          //color: Colors.red,
-          height: 120,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset("assets/images/nubank.png",
-                    height: 35,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 10,),
-                  Text("Erisvaldo",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            // Marcação de cor apenas para estudo de posição
+            color: Colors.red,
+            height: MediaQuery.of(context).size.height * .16,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("assets/images/nubank.png",
+                      height: 35,
                       color: Colors.white,
                     ),
-                  ),
-                ],
-              ),
-              Icon(Icons.expand_more,
-                color: Colors.white,
-              ),
-            ],
+                    SizedBox(width: 10,),
+                    Text("Erisvaldo",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(
+                  !showMenu ? Icons.expand_more : Icons.expand_less,
+                  color: Colors.white,
+                ),
+              ],
+            ),
           ),
         ),
       ],
