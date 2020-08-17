7cb6924701de1d43c46471447c1c2d78e5bb5add
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-17 08:31:25 -0300

    Criando Estrutura Inicial

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nubankinterface/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NuBank Interface',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
